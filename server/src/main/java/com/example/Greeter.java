b3a82dd170feaa5f2ff14830779d17663cae5b21
Jhon Robert Quintero <jrquinte@hotmail.com>
2019-10-27 16:37:24 -0500

    Cambios hechos

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,8 @@ public class Greeter {
   }
 
  /**
-  * Esta clase es de prueba
-  * @author: Jhon Robert Quintero
-  * @param someone
-  * @return someone
+  * @param someone the name of a person
+  * @return greeting string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
